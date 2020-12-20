4fc5558142226e832e5a026eacb2945523df329f
jun-pc <http.kj.com@gmail.com>
2020-12-20 09:17:52 +0900

    update physicaldeviceset.h カプセル化

--- cppvk/physicaldeviceset.h
+++ cppvk/physicaldeviceset.h
@@ -1,29 +1,32 @@
 #pragma once
 #include "vk.h"
 #include <cassert>
+#include <functional>
 
 namespace cppvk {
+
   struct PhysicalDeviceSet;
 
+  /// <summary>
+  ///
+  /// </summary>
   using ChoosePhysicalDeviceFuncInstanceBuilder = std::function<bool(PhysicalDeviceSet&)>;
 
-  struct PhysicalDeviceSet {
+  /// <summary>
+  ///
+  /// </summary>
+  class PhysicalDeviceSet {
+
+  private:
+
     VkPhysicalDevice device;
+
     VkPhysicalDeviceProperties props;
     VkPhysicalDeviceFeatures features;
     VkPhysicalDeviceMemoryProperties memory;
     ExtensionPropertiesList extensions;
     LayerPropertiesList validations;
     PhysicalDeviceQueueProps qprops;
-
-    PhysicalDeviceSet(VkPhysicalDevice gpu) :device(gpu) {
-      vkGetPhysicalDeviceProperties(gpu, &props);
-      vkGetPhysicalDeviceFeatures(gpu, &features);
-      vkGetPhysicalDeviceMemoryProperties(gpu, &memory);
-      extensions = getEnumeratePhysicalDeviceExtensions(gpu);
-      validations = getEnumerateDeviceLayer(gpu);
-      qprops = getEnumeratePhysicalDeviceQueueProps(gpu);
-    }
 
     PhysicalDeviceQueueProps getEnumeratePhysicalDeviceQueueProps(const VkPhysicalDevice gpu) {
       assert(gpu != VK_NULL_HANDLE);
@@ -50,6 +53,80 @@ namespace cppvk {
       LayerPropertiesList list(size);
       checkVk(vkEnumerateDeviceLayerProperties(gpu, &size, list.data()));
       return list;
+    }
+
+  public:
+
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="gpu"></param>
+    PhysicalDeviceSet(VkPhysicalDevice gpu) :device(gpu) {
+      vkGetPhysicalDeviceProperties(gpu, &props);
+      vkGetPhysicalDeviceFeatures(gpu, &features);
+      vkGetPhysicalDeviceMemoryProperties(gpu, &memory);
+      extensions = getEnumeratePhysicalDeviceExtensions(gpu);
+      validations = getEnumerateDeviceLayer(gpu);
+      qprops = getEnumeratePhysicalDeviceQueueProps(gpu);
+    }
+
+    /// <summary>
+    ///
+    /// </summary>
+    /// <returns></returns>
+    VkPhysicalDeviceProperties const& getProps() const {
+      return this->props;
+    }
+
+    /// <summary>
+    ///
+    /// </summary>
+    /// <returns></returns>
+    VkPhysicalDeviceFeatures const& getFeatures() const {
+      return this->features;
+    }
+
+    /// <summary>
+    ///
+    /// </summary>
+    /// <returns></returns>
+    VkPhysicalDeviceMemoryProperties const& getMemory() const {
+      return this->memory;
+    }
+
+    /// <summary>
+    ///
+    /// </summary>
+    /// <returns></returns>
+    ExtensionPropertiesList const& getExtensions() const {
+      return this->extensions;
+    }
+
+    /// <summary>
+    ///
+    /// </summary>
+    /// <returns></returns>
+    LayerPropertiesList const& getValidations() const {
+      return this->validations;
+    }
+
+    /// <summary>
+    ///
+    /// </summary>
+    /// <returns></returns>
+    PhysicalDeviceQueueProps const& getQprops() const {
+      return this->qprops;
+    }
+
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="pDevice"></param>
+    /// <param name="pConfiginfo"></param>
+    /// <param name="pAllocation"></param>
+    void CreateDevice(VkDevice *pDevice, const VkDeviceCreateInfo* pConfiginfo, const VkAllocationCallbacks* pAllocation) {
+      auto err = vkCreateDevice(this->device, pConfiginfo, pAllocation, pDevice);
+      checkVk(err);
     }
 
   };
